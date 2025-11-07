f091c6778067c73ee43cbccde51d808dad0df0de
Raul <rauldecarvalho2308@icloud.com>
2025-11-07 17:26:30 -0300

    remocao home e nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home'; 
-import { Login } from './components/login/login';
+
 import { RegisterCliente } from './components/register-cliente/register-cliente';
 import { RegisterEmpresa } from './components/register-empresa/register-empresa';   
 
 export const routes: Routes = [
     {
         path: '', component: Home
-    },
-
-    {
-        path:'login', component: Login
     },
 
     {
